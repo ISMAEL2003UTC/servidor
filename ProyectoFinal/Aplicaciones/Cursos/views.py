b8e553d68b7500c8a6807faf9a96bcaf50d08d3c
DEVjunior14 <p56367450@gmail.com>
2025-07-03 21:20:16 -0500

    modulo editar del crud estudiantes

--- ProyectoFinal/Aplicaciones/Cursos/views.py
+++ ProyectoFinal/Aplicaciones/Cursos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+import os
 
 from .models import Usuario, Tutor, Estudiante, Materia, Nivel, TutorMateria, Clase, Seguimiento, Pago, Valoracion, Ubicacion, MensajeClase
 
@@ -206,6 +207,41 @@ def eliminar_estudiantes(request,id):
     return redirect('/listar-usuarios')
 
 def editar_estudiantes(request,id):
-    pass
+    usuario=get_object_or_404(Usuario,id=id,rol='estudiante')
+    estudiante=get_object_or_404(Estudiante,usuario=usuario)
+    return render(request,'estudiantes/editarEstudiantes.html',{
+        'usuarios':usuario,
+        'estudiantes':estudiante
+    })
 def procesar_info_estudiantes(request):
-    pass
+    if request.method == 'POST':
+        id_usuario = request.POST['id']
+        usuario = Usuario.objects.get(id=id_usuario)
+        estudiante = Estudiante.objects.get(usuario=usuario)
+
+        # Actualizar campos del usuario
+        usuario.nombre = request.POST['nombre']
+        usuario.apellido = request.POST['apellido']
+        usuario.correo = request.POST['correo']
+        usuario.telefono = request.POST['telefono']
+        usuario.password_hash = request.POST['password']
+        usuario.fecha_registro = request.POST['fecha']
+        # Cambiar logo si se sube nuevo
+        if 'logo' in request.FILES:
+            if usuario.logo:
+                if os.path.isfile(usuario.logo.path):
+                    os.remove(usuario.logo.path)
+            usuario.logo = request.FILES['logo']
+        
+        usuario.save()
+
+        # Cambiar documento si se sube nuevo
+        if 'documento' in request.FILES:
+            if estudiante.documento:
+                if os.path.isfile(estudiante.documento.path):
+                    os.remove(estudiante.documento.path)
+            estudiante.documento = request.FILES['documento']
+        
+        estudiante.save()
+
+    return redirect('/listar-estudiantes')
